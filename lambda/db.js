f1f6685cbe783c06133392c33d2de08942389ef2
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-06-27 10:12:08 -0300

    feat: creating db configuration with aws dynamodb

---
+++ lambda/db.js
@@ -0,0 +1,86 @@
+const AWS = require("aws-sdk");
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+
+const { REGION, ACCESS_KEY_ID, SECRET_ACCESS_KEY } = require('./config');
+
+/**
+ * Custom DynamoDB persistence adapter.
+ */
+class CustomDynamoDbPersistenceAdapter extends ddbAdapter.DynamoDbPersistenceAdapter {
+    /**
+     * Get the attributes for the user.
+     * @param {*} requestEnvelope 
+     * @returns 
+     */
+    async getAttributes(requestEnvelope) {
+        const userId = requestEnvelope.context.System.user.userId;
+        const params = {
+            TableName: this.tableName,
+            Key: { 
+                'id': { S: userId },
+            }
+        };
+        const result = await this.dynamoDBClient.getItem(params).promise();
+        return result.Item ? this.mapItemToAttributes(result.Item) : {};
+    }
+
+    /**
+     * Save the attributes for the user.
+     * @param {*} requestEnvelope The request envelope.
+     * @param {*} attributes The attributes to save.
+     */
+    async saveAttributes(requestEnvelope, attributes) {
+        const userId = requestEnvelope.context.System.user.userId;
+        const item = {
+            'id': { S: userId },
+            'city': { S: attributes.city }
+        };
+        const params = {
+            TableName: this.tableName,
+            Item: item
+        };
+        await this.dynamoDBClient.putItem(params).promise();
+    }
+    
+    /**
+     * Map the attributes to the item.  
+     * @param {*} attributes The attributes to map.
+     * @returns Returns the item.
+     */
+    mapAttributesToItem(attributes) {
+        const item = {};
+        for (const key of Object.keys(attributes)) {
+            item[key] = { S: attributes[key].toString() };
+        }
+        return item;
+    }
+
+    /**
+     * Map the item to the attributes.
+     * @param {*} item 
+     * @returns 
+     */
+    mapItemToAttributes(item) {
+        const attributes = {};
+        for (const key of Object.keys(item)) {
+            attributes[key] = item[key].S;
+        }
+        return attributes;
+    }
+}
+
+/**
+ * The DynamoDB adapter instance.
+ */
+const ddbAdapterInstance = new CustomDynamoDbPersistenceAdapter({
+    tableName: 'city',
+    createTable: false,
+    dynamoDBClient: new AWS.DynamoDB({
+        apiVersion: 'latest',
+        region: REGION,
+        accessKeyId: ACCESS_KEY_ID,
+        secretAccessKey: SECRET_ACCESS_KEY
+    })
+});
+
+module.exports = { ddbAdapterInstance };
